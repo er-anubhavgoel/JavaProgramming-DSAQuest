7059b9e5efc52a05d6deba7a0ac3adab3dd0a254
Anubhav Goel <anubhavgoel0520@hotmail.com>
2025-03-26 17:25:44 +0530

    26 Mar 2025

--- sorting/InsertionSort_Desc.java
+++ sorting/InsertionSort_Desc.java
@@ -2,13 +2,13 @@ package sorting;
 
 import java.util.Arrays;
 
-public class InsertionSort {
-	// TODO: Sorting Array in Ascending/Non-Decreasing Order
+public class InsertionSort_Desc {
+	// TODO: Sorting Array in Descending/Decreasing Order
 	public static void main(String[] args) {
 		int[] arr = { 23, 12, 45, 23, 35, 46, 45, 6, 7, 65 };
 		System.out.println("Original Array is: " + Arrays.toString(arr));
 		insertionSort(arr);
-		System.out.println("\n<===After Sorting in Non-Decreasing Order===>\n");
+		System.out.println("\n<===After Sorting in Decreasing Order===>\n");
 		System.out.println("Sorted Array is: " + Arrays.toString(arr));
 	}
 
@@ -18,7 +18,7 @@ public class InsertionSort {
 		for (int i = 1; i < n; i++) {
 			int j = i;
 
-			while (j > 0 && a[j] < a[j - 1]) {
+			while (j > 0 && a[j] > a[j - 1]) {
 				int temp = a[j];
 				a[j] = a[j - 1];
 				a[j - 1] = temp;
